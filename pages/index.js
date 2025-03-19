ea920d33a7defd497038778a6b113f8bbbcdf8c2
Jhordan Pastorello <jhordanpastorello@MacBook-Pro-de-Jhordan.local>
2025-03-19 10:20:55 -0300

    sofrencia

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ function Home() {
                 <div className={Styles.cont}>
                     <h2 className={Styles.texto}>Não é meu primeiro contato com programação, nunca consegui seguir mais que 3 aulas kakakak,<br/> porém mas com uma didática dessas é realmente impossivel não dar seguimento a esse curso!!
                     </h2>
-                    <h3 className={Styles.texto}> Sofri pra fazer um contador de clique então CLICA AI SAFADO. </h3>
+                    <h3 className={Styles.texto}> Sofri pra fazer um contador de cliques então CLICA AI SAFADO. </h3>
                     <div>
                     <button id="meuBotao" className={Styles.button} onClick={() => setCount(count + 1)}> Aqui ó, CLICA! ------ {count}  </button>
                     </div>
