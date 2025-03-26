479879601c2f1363b9dff226a57809b68999f04b
Jhordan <pastorello_jhordan@icloud.com>
2025-03-26 10:44:16 -0300

    teste de imagem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 
 import Navbar from "../components/navbar";
 import Styles from "../styles/globals.module.css";
+import Image from "next/image"
 
 function Home() {
   return (
@@ -14,7 +15,17 @@ function Home() {
             Esse site tem o intuito de mostrar a minha evolução como programador seguido as instruções do curso.dev.
           </p>
         </div>
-        <div className={Styles.curso}></div>
+        <div className={Styles.curso}>
+          <Image
+            src="/image.png"
+            width={65}
+            height={65}
+            style={{
+              borderRadius: "10px"
+            }}
+            alt=""
+          />
+        </div>
       </div>
       <a className={Styles.a} href="/projeto1/projeto1">
         <button className={Styles.button2}>
@@ -23,7 +34,14 @@ function Home() {
           </h3>
         </button>
       </a>
-      <div className={Styles.person}></div>
+      <div className={Styles.person}>
+        <Image
+          src="/garoto.png"
+          width={295}
+          height={597}
+          alt=""
+        />
+      </div>
     </div>
   );
 }
